26b0fbaecd4ce3ad6fab9af28ee6763705ef6684
Wade-CQU <wade.cole@cqumail.com>
2024-02-21 20:03:09 +1100

    configure change to react router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path='/' element={<HomePage/>}></Route>
             <Route path='/details' element={<Details/>}></Route>
